07a86883c266caf037ef398b6bd1fdc793581bd1
macbook <haihoang1993@gmail.com>
2020-12-01 14:40:29 +0700

    git update

--- src/packages/users/route.js
+++ src/packages/users/route.js
@@ -5,6 +5,7 @@ import { isVerified } from "../../middleware/auth";
 const router = express.Router();
 
 router.get('/', controler.getOneUser);
+router.get('/validate', controler.getOneUser);
 router.put('/update', [isVerified], controler.updateOneUser);
 
 export default router;
